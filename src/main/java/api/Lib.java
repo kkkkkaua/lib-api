a32d982e02812ebafc434bed466f92b5d19e1b28
kkkkkaua <kauaworkmail@gmail.com>
2024-05-27 12:33:41 -0300

    primeiro commit

---
+++ src/main/java/api/Lib.java
@@ -0,0 +1,12 @@
+package api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lib {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lib.class, args);
+	}
+}
